4fd02a8f1a9506428af9c247620b62d9c4917a45
kissa1001 <kissa101295@gmail.com>
2016-02-28 19:09:58 -0500

    blah blah

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var _ = require('underscore');
+var _ = require('underscore-node');
 
 var Storage = function() {
     this.items = [];
@@ -36,29 +36,51 @@ app.post('/items', jsonParser, function(req, res) {
     res.status(201).json(item);
 });
 
-app.delete('/items/:id', function(request, response) {
-    var itemId = parseInt(request.params.id);
-    _.each(storage.items, function(item){
-    	if(item.id == itemId){
-    		response.status(201).json(item);
-    		storage.items.splice(itemId, 1);
-    		return response;
-    	}
-    	return response.sendStatus(404);
-    });
+app.delete('/items/:id', function(req, res) {
+    var itemId = req.params.id;
+    for (var i = 0 ; i < storage.items.length ; i++) {
+        if (storage.items[i].id == itemId) {
+            var item = storage.items[i];
+            res.status(201).json(itemId);
+            storage.items.splice(i, 1);
+            return res;
+        }
+    }
+
+    return response.sendStatus(404);
+    // var itemId = parseInt(req.params.id);
+    // _.each(storage.items, function(item){
+    //     if(item.id == itemId){
+    //         res.status(201).json(itemId);
+    //         storage.items.splice(item.id, 1);
+    //         return res;
+    //     }
+    // });
+    // return res.sendStatus(404);
+
 });
 
-app.put('/items/:id', jsonParser, function(request, response) {
-    var itemId = parseInt(request.params.id);
-    _.each(storage.items, function(item){
-    	if(item.id == itemId){
-    		item.name = request.body.name;
-    		response.status(201).json(item);
-    		return response;
-    	}
+app.put('/items/:id', jsonParser, function(req, res) {
+    var itemId = req.params.id;
+    for (var i = 0 ; i < storage.items.length ; i++) {
+        if (storage.items[i].id == itemId) {
+            storage.items[i].name = req.body.name;
+            res.status(201).json(storage.items[i]);
+            return res;
+        }
+    }
+
+    return response.sendStatus(404);
+    //var itemId = parseInt(req.params.id);
+    // _.each(storage.items, function(item){
+    //     if(item.id == itemId){
+    //         item.name = req.body.name;
+    //         res.status(201).json(item);
+    //         return res;
+    //     }
+    // });
+
+    // return res.sendStatus(404);
     });
 
-    storage.items.add(item.name);
-    response.status(201).end();
-});
 app.listen(process.env.PORT || 8080);
